0b3a88d9ab5a157a3cfcb174b840e001724e988d
mddionisie <miro9122@gmail.com>
2022-03-10 19:27:32 +0200

    Tasks 0-2

--- src/labs/week_03/collections/Exercise.java
+++ src/labs/week_03/collections/Exercise.java
@@ -1,6 +1,29 @@
 package labs.week_03.collections;
+import java.util.*;
 
 public class Exercise {
+
+    private static <T> void addElementAtFirstPosition(List<T> arr, T value)
+    {
+        arr.add(0, value);
+    }
+
+    private static <T> T getElementAtIndex(List<T> arr, int index) throws IndexOutOfBoundsException
+    {
+        return arr.get(index);
+    }
+
+    private static <T> void removeThirdElement(List<T> arr)
+    {
+        if(arr.size() < 3)
+        {
+            return;
+        }
+        else
+        {
+            arr.remove(3);
+        }
+    }
 
     public static void main(String[] args)
     {
